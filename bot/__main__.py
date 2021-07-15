6eedbc4c10c4d45c1c04f99c49d7715ba074c6dc
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-07-15 21:43:33 +0700

    🔀 Moved - bot instance Initialization code to bot.py - fetch config from firestore code to bot.py
    ✏ replcaed all add_cog func calls to a dynamic extension loader using
    walk_extensions generator from bot.utils.extensions
    
    ➕ Added a logging basicconfig to write all log entries into a log file
    
    🔥 Deleted on_message and on_ready event handlers

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,43 +1,21 @@
-import discord
-from discord.ext import commands
-import firebase_admin
-from firebase_admin import firestore
+import logging
 
+import coloredlogs
+
+from bot.bot import bot
 import bot.constants as constants
-from bot.exts.utils.announcements import Announcements
-from bot.exts.moderation.infraction.infractions import Infractions
-from bot.exts.utils.echo import EchoCommands
 from bot.utils.bot_prefix import BotPrefixHandler
+from bot.utils.extensions import walk_extensions
 
-firebase_admin.initialize_app(constants.Client.firebase_creds)
-db = firestore.client()
-config = [doc.to_dict() for doc in db.collection("config").stream() if doc.id == "main"][0]
-prefix: any = constants.Client.prefix if config["fixed_and_single_prefix"] else BotPrefixHandler.get_prefix
-bot: commands.bot = commands.Bot(command_prefix=prefix)
+logger = logging.getLogger(__name__)
+coloredlogs.install(level="DEBUG", logger=logger)
+logging.basicConfig(
+    filename="bot/logs/app.log",
+    filemode="w",
+    format="%(asctime)s %(name)s[%(process)d] %(levelname)s %(message)s",
+)
 
-
-@bot.event
-async def on_ready():
-    print("bot is ready")
-    dev_log_embed = discord.Embed(title="", description="", color=discord.Colour.green())
-    dev_log_embed.add_field(name="bot status", value="online", inline=False)
-    await bot.get_channel(constants.Channels.dev_log_channel).send(embed=dev_log_embed)
-
-bot.add_cog(Announcements(bot))
-bot.add_cog(EchoCommands(bot))
-bot.add_cog(Infractions(bot))
-
-@bot.event
-async def on_message(message):
-    if message.content.lower() in ("joe what", "joe who", "who is joe"):
-        await message.channel.send("oh god please no, don't please don't")
-
-    elif message.content.lower() in ("joe mama",):
-        await message.channel.send("why")
-
-    elif message.content.lower() in ("joe are you still alive", "rusty are you still alive"):
-        await message.channel.send("uhh, yes")
-
-    await bot.process_commands(message)
+for ext in walk_extensions():
+    bot.load_extension(ext)
 
 bot.run(constants.Client.token)
