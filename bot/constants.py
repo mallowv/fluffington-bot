a399c857ae4e45d2d62ac4ab21844d7020e6b3c3
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-07-15 21:48:15 +0700

    🎨 Pretified code - changed spacing - wrapped lines

--- bot/constants.py
+++ bot/constants.py
@@ -7,16 +7,19 @@ from bot.utils.get_yaml import get_yaml_val
 
 load_dotenv()
 
-class Client():
-    
-    name = "Lymba"
+
+class Client:
+    name = get_yaml_val("config.yml", "bot")["name"]
     prefix = get_yaml_val("config.yml", "bot")["prefix"]
     token: str = getenv("TOKEN")
     firebase_creds = credentials.Certificate(getenv("FIREBASE_ADMIN_CREDENTIALS_PATH"))
-    
-class Channels():
-    dev_log_channel: int = get_yaml_val("config.yml", "guild")["channels"]["dev_log"]
-    
-class Roles():
-    moderation_roles: list[str] = get_yaml_val("config.yml", "roles")["moderation_roles"]
-    
+
+
+class Channels:
+    devlog_channel: int = get_yaml_val("config.yml", "guild")["channels"]["dev_log"]
+
+
+class Roles:
+    moderation_roles: list[str] = get_yaml_val("config.yml", "roles")[
+        "moderation_roles"
+    ]
