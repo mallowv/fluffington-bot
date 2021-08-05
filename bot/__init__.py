c8d1fe667ac3aa57802f4ce663cb68bc566d5935
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-08-05 22:37:16 +0700

    ✏ Wrote initialization code for dev env and moved firebase_admin initialization here

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,1 +1,17 @@
+try:
+    from dotenv import load_dotenv
+
+    print("Found .env file, loading environment variables from it.")
+    load_dotenv()
+except ImportError:
+    print("Nevermind")
+    pass
+
+from os import getenv
+
+import firebase_admin
+firebase_admin.initialize_app(firebase_admin.credentials.Certificate(getenv("FIREBASE_ADMIN_CREDENTIALS_PATH")))
+
+from bot.constants import Client
+
 __version__ = "0.1.0"
