ebdcf9ae6d32d34e40d24dc01fddab492e92153d
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-08-15 10:26:13 +0700

    🎨 Pretified code

--- bot/exts/moderation/verification.py
+++ bot/exts/moderation/verification.py
@@ -13,11 +13,13 @@ coloredlogs.install(level="DEBUG", logger=logger)
 class Verification(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
-        
+
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member) -> None:
         logger.info(f"{member.id} joined {member.guild.name}")
-        await member.send(f"Hey {member.mention}, thanks for joining {member.guild.name}")
+        await member.send(
+            f"Hey {member.mention}, thanks for joining {member.guild.name}"
+        )
 
 
 def setup(bot: Bot) -> None:
