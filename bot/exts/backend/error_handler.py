38c6a6291a33bd82c0b0f1c8b3b6972f82d8c26b
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-08-23 19:57:07 +0700

    ➕⚠ Added CommandNotFound error handler

--- bot/exts/backend/error_handler.py
+++ bot/exts/backend/error_handler.py
@@ -1,12 +1,30 @@
-import logging
+from pathlib import Path
 
-import coloredlogs
+import yaml
+import random
+import discord
+from discord.ext import commands
+from discord.ext.commands import errors
 
 from bot.bot import Bot
 
-logger = logging.getLogger(__name__)
-coloredlogs.install(level="DEBUG", logger=logger)
+with Path("bot/resources/responses.yaml").open("r") as f:
+    NEGATIVE_RESPONSES = yaml.load(f, yaml.FullLoader).get("necative")
+
+
+class ErrorHandler(commands.Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error):
+        error_embed = discord.Embed(color=discord.Colour.red())
+        if isinstance(error, errors.CommandNotFound):
+            error_embed.title = random.choice(NEGATIVE_RESPONSES)
+            error_embed.description = "that command does not exist"
+            await ctx.send(embed=error_embed)
 
 
 def setup(bot: Bot):
-    logger.warning("Not yet implemented")
+    """load the ErrorHandler cog"""
+    bot.add_cog(ErrorHandler(bot))
