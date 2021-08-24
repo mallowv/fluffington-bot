defd2cc71e58a0d0e0fddbba696a7754cca6f2d6
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-08-24 20:27:56 +0700

    ♻ Refactored code

--- bot/utils/bot_prefix.py
+++ bot/utils/bot_prefix.py
@@ -1,26 +1,30 @@
-import firebase_admin
 from firebase_admin import firestore
+from discord.ext import commands
 
-from bot.constants import Client
+from bot import constants
 
-class BotPrefixHandler():
+
+class BotPrefixHandler:
     """
     handles bot prefixes across servers
     """
-    
-    #firebase_admin.initialize_app(Client.firebase_creds)
 
-    def get_prefix(self, message):
-        db = firestore.client()
-        prefixes_ref = db.collection("prefixes")
-        docs = prefixes_ref.stream()
-        prefix = ""
-        
-        for doc in docs:
-            if doc.id == str(message.guild.id):
-                prefix = doc.to_dict()["prefix"]
-                
-            else:
-                continue
+    # firebase_admin.initialize_app(Client.firebase_creds)
 
-        return prefix
+    @staticmethod
+    def get_prefix(bot, message):
+        if not bot.static_prefix:
+            db = firestore.client()
+            prefixes_ref = db.collection("prefixes")
+            prefix = [
+                doc.to_dict()
+                for doc in prefixes_ref.stream()
+                if doc.id == str(message.guild.id)
+            ][0].get("prefix")
+
+            return (
+                commands.when_mentioned_or(prefix)(bot, message)
+                if prefix
+                else commands.when_mentioned_or(constants.Client.prefix)(bot, message)
+            )
+        return commands.when_mentioned_or(constants.Client.prefix)(bot, message)
