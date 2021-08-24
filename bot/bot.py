ffb4d60a83c48a0cc268705884ebbd7cbd31bd72
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-08-24 20:17:48 +0700

    ♻ Refactored code ➕ added startup_time var

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 import coloredlogs
+import arrow
 import discord
 from discord import DiscordException, Embed
 from discord.ext import commands
@@ -10,7 +11,6 @@ from firebase_admin import firestore
 
 import bot.constants as constants
 from bot.utils.bot_prefix import BotPrefixHandler
-from bot.utils.converters import Prefix
 
 logger = logging.getLogger(__name__)
 coloredlogs.install(level="DEBUG", logger=logger)
@@ -25,12 +25,25 @@ class Bot(commands.bot.Bot):
     that the upload was successful. See the `mock_in_debug` decorator for further details.
     """
 
+    startup_time = arrow.utcnow()
     name = constants.Client.name
 
     def __init__(self, **kwargs):
         super(Bot, self).__init__(**kwargs)
         self._guild_available = asyncio.Event()
         self.loop.create_task(self.send_log(self.name, "Connected!"))
+        self.debug = True
+        self.static_prefix = self.debug
+
+    @classmethod
+    def create(cls):
+        intents = discord.Intents.default()
+        intents.members = True
+        return cls(
+            command_prefix=BotPrefixHandler.get_prefix,
+            activity=discord.Game(name=f"Commands: {constants.Client.prefix}help"),
+            intents=intents,
+        )
 
     def add_cog(self, cog):
         """
@@ -40,6 +53,14 @@ class Bot(commands.bot.Bot):
         """
         super(Bot, self).add_cog(cog)
         logger.info(f"Cog loaded: {cog.qualified_name}")
+
+    def load_extension(self, name, *, package=None):
+        super(Bot, self).load_extension(name, package=package)
+        logger.info(f"Extension loaded: {name}")
+
+    def unload_extension(self, name, *, package=None):
+        super(Bot, self).unload_extension(name, package=package)
+        logger.info(f"Extension unloaded: {name}")
 
     async def send_log(
         self, title: str, details: str = None, *, icon: str = None
@@ -66,31 +87,3 @@ class Bot(commands.bot.Bot):
 
         await devlog.send(embed=embed)
         logger.info(f"{self.name} Connected!")
-
-    async def wait_until_guild_available(self) -> None:
-        """
-        Wait until the PyDis guild becomes available (and the cache is ready).
-        The on_ready event is inadequate because it only waits 2 seconds for a GUILD_CREATE
-        gateway event before giving up and thus not populating the cache for unavailable guilds.
-        """
-        await self._guild_available.wait()
-
-
-firebase_admin.initialize_app(constants.Client.firebase_creds)
-db = firestore.client()
-config = [
-    doc.to_dict() for doc in db.collection("config").stream() if doc.id == "main"
-][0]
-prefix: Prefix = (
-    constants.Client.prefix
-    if config["fixed_and_single_prefix"]
-    else BotPrefixHandler.get_prefix
-)
-intents = discord.Intents.default()
-intents.members = True
-
-bot: Bot = Bot(
-    command_prefix=prefix,
-    activity=discord.Game(name=f"Commands: {prefix}help"),
-    intents=intents,
-)
