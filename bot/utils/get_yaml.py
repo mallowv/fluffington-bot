f20b9d89861a7ac94dc7f9f607f6fbf21609574d
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-09-06 10:20:11 +0700

    Changed to dynamic config loader

--- bot/utils/get_yaml.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from yaml.loader import UnsafeLoader
-
-
-def get_yaml_val(config_file: str, string: str):
-    with open(config_file) as config:
-        config = yaml.load(config, Loader=UnsafeLoader)
-
-    return config[string]
