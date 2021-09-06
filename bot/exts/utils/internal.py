5e0b8b748494560d946e01fc5478795a1f223823
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-09-06 10:23:53 +0700

    ➕ Made commandstats command

--- bot/exts/utils/internal.py
+++ bot/exts/utils/internal.py
@@ -23,6 +23,8 @@ class Internals(commands.Cog):
         self.bot = bot
         self.socket_events: Counter = Counter()
         self.socket_event_total = 0
+        self.command_usages: Counter = Counter()
+        self.command_usage_total = 0
         self.socket_since: utime = datetime.utcnow()
 
     @commands.Cog.listener()
@@ -31,6 +33,11 @@ class Internals(commands.Cog):
         if event_type := msg.get("t"):
             self.socket_event_total += 1
             self.socket_events[event_type] += 1
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx: commands.Context):
+        self.command_usage_total += 1
+        self.command_usages[ctx.command.name] += 1
 
     @commands.group(name="internal", aliases=["int"])
     @commands.has_any_role(*Roles.moderation_roles)
@@ -57,6 +64,25 @@ class Internals(commands.Cog):
             stats_embed.add_field(name=event_type, value=f"{count:,}", inline=True)
 
         await ctx.send(embed=stats_embed)
+
+    @internal_group.command(name="commandstats", aliases=("cmdstats",))
+    @commands.has_any_role(*Roles.moderation_roles)
+    async def commandstats(self, ctx: commands.Context) -> None:
+        """Get command usage information"""
+        running_s = (datetime.utcnow() - self.socket_since).total_seconds()
+
+        per_s = self.socket_event_total / running_s
+
+        command_stats_embed = discord.Embed(
+            title="Command statistics",
+            description=f"Receiving {per_s:0.2f} command invocations per second.",
+            color=discord.Color.blurple(),
+        )
+
+        for command_name, count in self.command_usages.most_common(25):
+            command_stats_embed.add_field(name=command_name, value=f"{count:,}", inline=True)
+
+        await ctx.send(embed=command_stats_embed)
 
 
 def setup(bot: Bot) -> None:
