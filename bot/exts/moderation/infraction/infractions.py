1286a176f942151aa8c06007bbd44156c3db6ef6
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-09-09 07:25:40 +0700

    ➕ Added ban, unban, kick, and mute commands

--- bot/exts/moderation/infraction/infractions.py
+++ bot/exts/moderation/infraction/infractions.py
@@ -1,22 +1,33 @@
 import datetime
+import asyncio
+from typing import Optional
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 
-import bot.constants as constants
+from bot.bot import Bot
+from bot.constants import Roles
+from bot.utils.converters import FetchedMember
+from bot.utils.time import discord_timestamp
+
 
 class Infractions(commands.Cog):
     """
     Apply and pardon infractions on users for moderation purposes.
     """
-    
+
     def __init__(self, bot: commands.bot) -> None:
         super().__init__()
         self.bot = bot
 
-    @commands.has_permissions(ban_members = True)
-    @commands.command(name="ban")
-    async def ban(self, ctx: commands.context, member: discord.User = None, *, reason: str = None):
+    @staticmethod
+    async def apply_ban(
+        ctx, member: FetchedMember, reason: Optional[str] = None, **kwargs
+    ):
+        """
+        Apply a ban infarction to a user
+        """
         if member is None or member == ctx.author:
             await ctx.send("You cannot ban yourself")
             return
@@ -26,3 +37,71 @@ class Infractions(commands.Cog):
         await member.send(message)
         await ctx.guild.ban(member, reason=reason)
         await ctx.channel.send(f"{member.mention} has been banned for {reason}")
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(name="ban")
+    async def ban(
+        self, ctx: commands.context, member: FetchedMember = None, *, reason: str = None
+    ):
+        """
+        Ban a user
+        """
+        await self.apply_ban(ctx, member, reason)
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command(name="unban")
+    async def unban(self, ctx: commands.context, member: FetchedMember):
+        """
+        Pardon a user from a ban
+        """
+        await ctx.guild.unban(member)
+        await ctx.send(f"{member.mention} has been unbanned :tada:")
+
+    @commands.has_permissions(kick_members=True)
+    @commands.command(name="kick")
+    async def kick(
+        self, ctx: commands.context, member: FetchedMember, *, reason: str = None
+    ):
+        """
+        Kick a user out
+        """
+        if member is None or member == ctx.author:
+            await ctx.send("You cannot kick yourself")
+            return
+        if reason is None:
+            reason = "For being a jerk!"
+        message = f"You have been kicked from {ctx.guild.name} for {reason}"
+        await member.send(message)
+        await member.kick(reason=reason)
+        await ctx.send(f"{member.mention} has been kicked for {reason}")
+
+    @commands.command(name="mute")
+    @commands.has_any_role(*Roles.moderation_roles)
+    async def mute(
+        self,
+        ctx: commands.context,
+        member: FetchedMember,
+        duration: Optional[int] = 3600,
+        *,
+        reason: str,
+    ):
+        muted = get(member.guild.roles, name="Muted")
+        await member.add_roles(muted, reason=reason)
+        mute_duration = (
+            discord_timestamp(datetime.datetime.utcnow() + datetime.timedelta(seconds=duration))
+        )
+        mute_message = f"{mute_duration} ({datetime.timedelta(seconds=duration)})"
+        await ctx.send(
+            f":incoming_envelope: :ok_hand: applied **mute** to {member.mention} until {mute_message}"
+        )
+        await member.send(f"you have been muted in {member.guild.name} for {reason}")
+        await asyncio.sleep(duration)
+        await member.remove_roles(muted)
+        await ctx.send(
+            f":incoming_envelope: :ok_hand: pardoned infraction **mute** for {member.mention}"
+        )
+
+
+def setup(bot: Bot) -> None:
+    """load the Infractions cog"""
+    bot.add_cog(Infractions(bot))
