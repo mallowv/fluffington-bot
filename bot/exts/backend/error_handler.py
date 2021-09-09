5b8a0c22fc7e5bcf2d2984488d04b67309c31122
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-09-09 14:30:23 +0700

    ➕ Added error replies

--- bot/exts/backend/error_handler.py
+++ bot/exts/backend/error_handler.py
@@ -1,15 +1,11 @@
-from pathlib import Path
-
-import yaml
 import random
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import errors
 
 from bot.bot import Bot
-
-with Path("bot/resources/responses.yaml").open("r") as f:
-    NEGATIVE_RESPONSES = yaml.load(f, yaml.FullLoader).get("necative")
+from bot.constants import ERROR_REPLIES
 
 
 class ErrorHandler(commands.Cog):
@@ -20,11 +16,11 @@ class ErrorHandler(commands.Cog):
     async def on_command_error(self, ctx: commands.Context, error):
         error_embed = discord.Embed(color=discord.Colour.red())
         if isinstance(error, errors.CommandNotFound):
-            error_embed.title = random.choice(NEGATIVE_RESPONSES)
+            error_embed.title = random.choice(ERROR_REPLIES)
             error_embed.description = f"{ctx.invoked_with} command does not exist"
             await ctx.send(embed=error_embed)
             return
-        error_embed.title = random.choice(NEGATIVE_RESPONSES)
+        error_embed.title = random.choice(ERROR_REPLIES)
         error_embed.description = str(error)
         await ctx.send(embed=error_embed)
 
