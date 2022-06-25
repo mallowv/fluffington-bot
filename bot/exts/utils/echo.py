f3123add6308e2c9607730f3330e46804e81b6dd
Mallow <72262190+mallowv@users.noreply.github.com>
2022-06-25 20:06:24 +0700

    Changed default converter to custom one

--- bot/exts/utils/echo.py
+++ bot/exts/utils/echo.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import discord
 from discord.ext import commands
 from discord.ext.commands.converter import TextChannelConverter
 from discord import Embed, TextChannel
@@ -10,7 +11,7 @@ from bot.constants import Roles
 
 class EchoCommands(commands.Cog):
     """
-    Apply and pardon infractions on users for moderation purposes.
+    Echo...stuff
     """
 
     def __init__(self, bot: Bot) -> None:
@@ -20,7 +21,7 @@ class EchoCommands(commands.Cog):
     @commands.command(name="echo", aliases=("print",))
     @commands.has_any_role(*Roles.moderation_roles)
     async def echo_command(
-        self, ctx: commands.Context, channel: Optional[TextChannel], *, text: str
+        self, ctx: commands.Context, channel: Optional[TextChannelConverter], *, text: str
     ) -> None:
         """Repeat the given message in either a specified channel or the current channel."""
         if channel is None:
@@ -33,10 +34,14 @@ class EchoCommands(commands.Cog):
     @commands.command(name="embed")
     @commands.has_any_role(*Roles.moderation_roles)
     async def embed_command(
-        self, ctx: commands.Context, channel: Optional[TextChannel], *, text: str
+        self,
+        ctx: commands.Context,
+        channel: Optional[TextChannel],
+        colour: Optional[discord.Colour] = discord.Colour.blurple(),
+        *, text: str
     ) -> None:
         """Send the input within an embed to either a specified channel or the current channel."""
-        embed = Embed(description=text)
+        embed = Embed(description=text, colour=colour)
 
         if channel is None:
             await ctx.send(embed=embed)
