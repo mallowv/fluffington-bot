06fe6596e8a36f4f48cc61320811aa755278d5dd
Mallow <72262190+mallowv@users.noreply.github.com>
2022-06-25 20:04:32 +0700

    ♻ Refactored entire defcon system

--- bot/exts/moderation/defcon.py
+++ bot/exts/moderation/defcon.py
@@ -1,17 +1,71 @@
+from datetime import datetime
+import logging
+
 import discord
 from discord.ext import commands
+from dateutil.relativedelta import relativedelta
 
 from bot.bot import Bot
-from bot.constants import Roles
+from bot.constants import Roles, Colours
+from bot.database.models import Guild
+
+logger = logging.getLogger(__name__)
+REJECTION_MESSAGE = """
+Hi, {user} - Thanks for your interest in our server!
+Due to a current (or detected) cyberattack on our community, we've limited access to the {guild} server
+for new accounts. Since
+your account is relatively new, we're unable to provide access to the server at this time.
+Even so, thanks for joining! We're very excited at the possibility of having you here, and we hope that this situation
+will be resolved soon.
+"""
 
 
 class DefCon(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
+        self.treshold = relativedelta(days=5)
+        self.shutdowned = {}
+
+    @property
+    def mod_log(self):
+        """Get the currently loaded ModLog cog instance."""
+        return self.bot.get_cog("ModLog")
+
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        ...
+        now = datetime.utcnow()
+        if self.shutdowned.get(member.guild.id):
+            if now - member.created_at < (now + self.treshold - now):
+                msg_sent = False
+
+                try:
+                    await member.send(
+                        REJECTION_MESSAGE.format(
+                            user=member.mention, guild=member.guild.name
+                        )
+                    )
+
+                    msg_sent = True
+                except Exception:
+                    logger.exception(
+                        f"Unable to send rejection message to user: {member}"
+                    )
+
+                await member.kick(reason="DEFCON active, user is too new")
+                message = f"{format_user(member)} was denied entry because their account is too new."
+
+                if not message_sent:
+                    message = f"{message}\n\nUnable to send rejection message via DM; they probably have DMs disabled."
+
+                await self.mod_log.send_log_message(
+                    "https://cdn.discordapp.com/emojis/472475292078964738.png",
+                    Colours.soft_red,
+                    "Entry denied",
+                    message,
+                    member.avatar,
+                    Guild.get(member.guild.id).server_log_channel,
+                )
 
     @commands.group(name="defcon", aliases=("dc",))
     async def defcon_group(self, ctx: commands.Context):
@@ -25,6 +79,7 @@ class DefCon(commands.Cog):
         role = ctx.guild.default_role
         permissions = role.permissions
 
+        self.shutdowned[ctx.guild.id] = True
         permissions.update(send_messages=False, add_reactions=False, connect=False)
         await role.edit(reason="DEFCON shutdown", permissions=permissions)
         await ctx.send(":white_check_mark::lock:  server locked down")
@@ -36,6 +91,7 @@ class DefCon(commands.Cog):
         role = ctx.guild.default_role
         permissions = role.permissions
 
+        self.shutdowned[ctx.guild.id] = False
         permissions.update(send_messages=True, add_reactions=True, connect=True)
         await role.edit(reason="DEFCON unshutdown", permissions=permissions)
         await ctx.send(":white_check_mark::unlock: server reopened")
